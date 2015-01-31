7b32b4aea87c0c1808c428001415534810ddcd7f
DuhProgrammer13 <duhvanry@gmail.com>
2015-01-30 23:52:52 -0700

    More shtuff

--- core/src/com/desitum/twist/TwistGame.java
+++ core/src/com/desitum/twist/TwistGame.java
@@ -1,18 +1,19 @@
 package com.desitum.twist;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.desitum.twist.screens.MainScreen;
 
-public class TwistGame extends ApplicationAdapter {
+public class TwistGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
         ScreenAdapter mainScreen = new MainScreen();
+        this.setScreen(mainScreen);
 	}
 
 	@Override
