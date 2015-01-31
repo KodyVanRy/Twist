e5b2f4d82f53b16a2e4c43d8920859765bc3f446
DuhProgrammer13 <duhvanry@gmail.com>
2015-01-30 23:22:57 -0700

    More shtuff

--- core/src/com/desitum/twist/objects/MenuButton.java
+++ core/src/com/desitum/twist/objects/MenuButton.java
@@ -1,14 +1,19 @@
 package com.desitum.twist.objects;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 /**
  * Created by kody on 1/30/15.
  */
-public class MenuButton {
+public class MenuButton extends Sprite {
 
     private String command;
     private String text;
 
-    public MenuButton(String command, String text, int locX, int locY) {
+    private static int SIZE_X;
+    private static int SIZE_Y;
+
+    public MenuButton(String command, String text, float locX, float locY) {
         this.command = command;
         this.text = text;
     }
