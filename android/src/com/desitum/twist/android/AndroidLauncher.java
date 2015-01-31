527d96ef633363530d84c03dc81c64e9820babdf
DuhProgrammer13 <duhvanry@gmail.com>
2015-01-30 21:11:29 -0700

    Initial commit

---
+++ android/src/com/desitum/twist/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.desitum.twist.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.desitum.twist.TwistGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TwistGame(), config);
+	}
+}
