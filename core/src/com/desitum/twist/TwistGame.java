f975ccd3ea079145a75e04eb88721af8597027c8
DuhProgrammer13 <duhvanry@gmail.com>
2015-01-30 22:13:06 -0700

    Added Main screen

--- core/src/com/desitum/twist/TwistGame.java
+++ core/src/com/desitum/twist/TwistGame.java
@@ -1,10 +1,10 @@
 package com.desitum.twist;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.desitum.twist.com.desitum.twist.screens.MainScreen;
 
 public class TwistGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -12,16 +12,11 @@ public class TwistGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        ScreenAdapter mainScreen = new MainScreen();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 }
---
+++ core/src/com/desitum/twist/com/desitum/twist/screens/MainScreen.java
@@ -0,0 +1,37 @@
+package com.desitum.twist.com.desitum.twist.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Created by kody on 1/30/15.
+ */
+public class MainScreen extends ScreenAdapter {
+
+    @Override
+    public void render (float delta) {
+    }
+
+    @Override
+    public void resize (int width, int height) {
+    }
+
+    @Override
+    public void show () {
+    }
+
+    @Override
+    public void hide () {
+    }
+
+    @Override
+    public void pause () {
+    }
+
+    @Override
+    public void resume () {
+    }
+
+    @Override
+    public void dispose () {
+    }
+}
