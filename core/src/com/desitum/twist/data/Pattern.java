b504acd4588936559243e7c456aa3cda7e1f1eae
Zmyth97 <zmyth97@gmail.com>
2015-02-05 12:27:46 -0700

    SMUFF

--- core/src/com/desitum/twist/data/Pattern.java
+++ core/src/com/desitum/twist/data/Pattern.java
@@ -12,12 +12,18 @@ import java.util.Random;
 public class Pattern {
 
     private static ArrayList<Bar> pattern1;
+    private static ArrayList<Bar> pattern2;
 
     private static ArrayList<ArrayList<Bar>> patterns;
 
     public static void loadPatterns(){
         patterns = new ArrayList<ArrayList<Bar>>();
 
+
+    }
+
+    public static void loadPattern1()
+    {
         pattern1 = new ArrayList<Bar>();
         Bar pattern1_bar1 = new Bar(4, 4, Bar.HORIZONTAL, new float[]{1, 0}, Assets.purpleBarTexture);
         pattern1_bar1.setBarStoppingPointRight(6);
@@ -33,6 +39,23 @@ public class Pattern {
         pattern1.add(pattern1_bar4);
 
         patterns.add(pattern1);
+    }
+
+    public static void loadPattern2()
+    {
+        pattern2 = new ArrayList<Bar>();
+        Bar pattern2_bar1 = new Bar(4, 4, Bar.HORIZONTAL, new float[]{1, 0}, Assets.redBarTexture);
+        pattern2_bar1.setBarStoppingPointRight(6);
+        Bar pattern2_bar2 = new Bar(3, 4, Bar.HORIZONTAL, new float[]{3, 1.5f}, Assets.redBarTexture);
+        pattern2_bar2.setBarStoppingPointLeft(4);
+        Bar pattern2_bar3 = new Bar(4, 4, Bar.VERTICAL, new float[]{2, 8}, Assets.redBarTexture);
+        pattern2_bar3.setBarStoppingPointRight(6);
+        Bar pattern2_bar4 = new Bar(3, 4, Bar.HORIZONTAL, new float[]{6, 6.5f}, Assets.redBarTexture);
+        pattern2_bar4.setBarStoppingPointLeft(4);
+        pattern1.add(pattern2_bar1);
+        pattern1.add(pattern2_bar2);
+        pattern1.add(pattern2_bar3);
+        pattern1.add(pattern2_bar4);
     }
 
     public static ArrayList<Bar> getRandomPattern(float y){
